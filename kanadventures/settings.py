396da2b9af6872162cf74e23c45a92c5c0c7625b
Sandro V Lobao <svlobao@bol.com.br>
2025-06-30 21:03:07 +0900

    chore: listed all apps on settings.py

--- kanadventures/settings.py
+++ kanadventures/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # custom apps
+    "common",
+    "flashcards",
+    "phrases",
+    "progress",
+    "quizzes",
+    "users",
+    "vocabulary",
 ]
 
 MIDDLEWARE = [
